3e9f44518ec3e5c8c33e200224c54b54c3b5e2c7
Alisson Chiquitto <chiquitto@gmail.com>
2016-09-18 15:18:49 -0300

    Endereco

--- src/br/com/chiquitto/aula/jdbcescola/dao/EnderecoDao.java
+++ src/br/com/chiquitto/aula/jdbcescola/dao/EnderecoDao.java
@@ -1,9 +1,14 @@
 package br.com.chiquitto.aula.jdbcescola.dao;
 
 import br.com.chiquitto.aula.jdbcescola.Conexao;
+import br.com.chiquitto.aula.jdbcescola.exception.RowNotFoundException;
 import br.com.chiquitto.aula.jdbcescola.vo.Endereco;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -21,8 +26,76 @@ public class EnderecoDao extends AbstractDao {
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
-        
+
         System.out.println(endereco.getIdpessoa());
+    }
+
+    public List<Endereco> getAll() {
+        List<Endereco> enderecos = new ArrayList<>();
+
+        try {
+            Statement st = Conexao.getConexao().createStatement();
+            ResultSet rs = st.executeQuery("Select idpessoa, idcidade, logradouro, numero From endereco");
+
+            while (rs.next()) {
+                enderecos.add(recordset2Vo(rs));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return enderecos;
+    }
+    
+    public List<Endereco> getByIdpessoa(int idpessoa) {
+        List<Endereco> enderecos = new ArrayList<>();
+
+        try {
+            String sql = "Select idpessoa, idcidade, logradouro, numero From endereco Where idpessoa = ?";
+            
+            PreparedStatement st = Conexao.getConexao().prepareStatement(sql);
+            st.setInt(1, idpessoa);
+            ResultSet rs = st.executeQuery();
+
+            while (rs.next()) {
+                enderecos.add(recordset2Vo(rs));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return enderecos;
+    }
+
+    public Endereco getOne(int idendereco) throws RowNotFoundException {
+        try {
+            String sql = "Select idpessoa, idcidade, logradouro, numero From endereco Where idendereco = ?";
+            PreparedStatement st = Conexao.getConexao().prepareStatement(sql);
+            st.setInt(1, idendereco);
+
+            ResultSet rs = st.executeQuery();
+
+            if (!rs.next()) {
+                return recordset2Vo(rs);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        throw new RowNotFoundException();
+    }
+
+    protected Endereco recordset2Vo(ResultSet rs) throws SQLException {
+        Endereco endereco = new Endereco();
+        endereco.setIdpessoa(rs.getInt("idpessoa"));
+        endereco.setIdcidade(rs.getInt("idcidade"));
+        endereco.setLogradouro(rs.getString("logradouro"));
+        endereco.setNumero(rs.getString("numero"));
+
+        return endereco;
     }
 
     public void salvar(Endereco endereco) {
